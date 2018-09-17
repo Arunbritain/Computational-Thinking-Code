5b6dc0ea0d259c9ffb33d9845841e535b113747d
Arunbritain <43275080+Arunbritain@users.noreply.github.com>
2018-09-17 15:50:52 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("Enter two numbers to add\n");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("The answer is:%d",c);
+getch();
+}
