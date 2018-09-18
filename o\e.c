a568a5a5b92ef3e4127d81786001ca51d4822679
Arunbritain <43275080+Arunbritain@users.noreply.github.com>
2018-09-18 12:13:04 +0530

    Create o\e.c

---
+++ o\e.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+Void main()
+{
+ int a;
+ clrscr();
+ printf("INPUT\n");
+ scanf("%d".&a);
+ if(a>=0)
+ {
+  if(a>0)
+  {
+   printf("POSITIVE\n");
+  }
+  else
+  {
+   printf("ZERO");
+  }
+ }
+else
+{
+ printf("NEGATIVE"\n);
+}
