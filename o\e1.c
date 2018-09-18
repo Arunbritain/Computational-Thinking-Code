c081994fe79fcdcd28aecfa343f00b7db738a5ad
Arunbritain <43275080+Arunbritain@users.noreply.github.com>
2018-09-18 12:36:59 +0530

    Create o\e1.c

---
+++ o\e1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int n;
+ clrscr();
+ printf("Input: ");
+ scanf("%d",&n);
+ printf("Output:\n");
+ if(a>0)
+ {
+  if((a%2)==0)
+  {
+   printf("Even");
+  }
+  else
+  {
+   printf("Odd");
+  }
+ }
+ else
+ {
+  printf("invalid");
+ }
+ getch();
+ }
