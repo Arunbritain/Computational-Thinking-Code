947a487e78c8d5b1b069184678c4c9c00aee3ead
Arunbritain <43275080+Arunbritain@users.noreply.github.com>
2019-01-03 14:29:34 +0530

    Create largestnum.py

---
+++ largestnum.py
@@ -0,0 +1,11 @@
+print("Input:")
+a=int(input("\t"))
+
+b=int(input("\t"))
+c=int(input("\t"))
+if((a>b)and(a>c)):
+  print("Output: \n",a)
+elif((b>c)and(b>a)):
+  print("Output: \n",b)
+else:
+  print("Output: \n",c)
