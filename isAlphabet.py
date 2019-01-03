1fb11bdd79e5d42cfd947a362030dbac1de95c88
Arunbritain <43275080+Arunbritain@users.noreply.github.com>
2019-01-03 14:16:10 +0530

    Create isAlphabet.py

---
+++ isAlphabet.py
@@ -0,0 +1,7 @@
+l=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','f','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+c=input('Input:')
+print(c)
+if c in l:
+  print("Alphabet")
+else:
+  print("NO")
